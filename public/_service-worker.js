30f2abd755818343aebac30cf3e7ec1d187cfc8b
Djek LU(kraken) <djeklu@gmail.com>
2022-05-30 18:30:19 +0300

    fix APP_VER

--- public/_service-worker.js
+++ public/_service-worker.js
@@ -1,7 +1,7 @@
 // To clear cache on devices, always increase APP_VER number after making changes.
 // The app will serve fresh content right away or after 2-3 refreshes (open / close)
 var APP_NAME = 'My.HomeNet.Online';
-var APP_VER = '4.8.3';
+var APP_VER = '4.8.4';
 var CACHE_NAME = APP_NAME + '-' + APP_VER;
 
 // Files required to make this app work offline.
